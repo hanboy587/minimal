935c63788ce2fde52efd2dc773d267823eaf69e4
unknown <hanboy587@gmail.com>
2023-12-06 14:24:15 +0900

    grid print success

--- demo/src/main/java/com/react/demo/controller/UserController.java
+++ demo/src/main/java/com/react/demo/controller/UserController.java
@@ -4,9 +4,11 @@ import com.react.demo.repository.UserRepsitory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor //service 사용 가능
@@ -23,6 +25,11 @@ public class UserController {
     @GetMapping("/api/getusernames")
     public List<String> getAllUserNames() {
         return userRepsitory.getAllUserNames();
+    }
+
+    @GetMapping("/api/getUserList")
+    public List<Map<String, Object>> getUserList() {
+        return userRepsitory.getAllUsers();
     }
 
 }
--- demo/src/main/java/com/react/demo/repository/UserRepsitory.java
+++ demo/src/main/java/com/react/demo/repository/UserRepsitory.java
@@ -4,8 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.jdbc.core.JdbcTemplate;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 //import java.util.ArrayList;
 
 @Repository
@@ -18,5 +17,26 @@ public class UserRepsitory {
         usernameList.addAll(jdbcTemplate.queryForList("select userName from users;", String.class));
 
         return usernameList;
+    }
+
+//    public Map<String, Object> getAllUsers() {
+//        List<Map<String, Object>> resultList = jdbcTemplate.queryForList("select userNo, userName from users;");
+//
+//        Map<String, Object> userList = new HashMap<>();
+//
+//        for (Map<String, Object> result : resultList) {
+//            String userNo = result.get("userNo").toString();
+//            String userName = result.get("userName").toString();
+//            userList.put(userNo, userName);
+//        }
+//
+//        return userList;
+//    }
+
+    public List<Map<String, Object>> getAllUsers() {
+        List<Map<String, Object>> userList = new ArrayList<>();
+        userList.addAll(jdbcTemplate.queryForList("select userNo, userName from users"));
+
+        return userList;
     }
 }
